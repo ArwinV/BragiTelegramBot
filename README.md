457200d7432f29e590f551df7246481750bcce4f
Arwin Verhoeven <arwin_verhoeven@hotmail.com>
2021-05-06 18:07:17 +0200

    Now the host/container reboots when an error occurs because this fixes the error. Also added service file.

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ You need the following packages:
 - unidecode
 
 In order to use the bot you have to put your token in a file called 'token.txt' and your user id in a file called admin_id.txt in the same directory as the bragi.py file.
+
+In order to autostart the bot:
+- Copy bragi.service to /etc/systemd/system/
+- Run systemctl daemon-reload
+- Enable the service with systemctl enable bragi
+
+The bot can also automatically be started with:
+systemctl start bragi
--- bragi.py
+++ bragi.py
@@ -18,6 +18,7 @@ from datetime import datetime
 import unicodedata
 from unidecode import unidecode
 import json
+import os
 
 # Enable logging
 logging.root.setLevel(logging.NOTSET)
@@ -26,6 +27,12 @@ logger = logging.getLogger(__name__)
 
 # Url Regex
 URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+def error_printing(update: Update, context: CallbackContext, name) -> None:
+    logging.error("Failed to print message")
+    update.message.reply_text("Failed to print your message :( My shitty usb serial cable sometimes temporarily stops working which messes things up. The container hosting this software will now be rebooted which fixes this problem. Please send your message again because I won't see it otherwise.")
+    context.bot.send_message(data['admin_id'], text="Failed to print a text message from {}. The container will be rebooted.".format(name))
+    os.system('reboot')
 
 def store_data() -> None:
     """Store data to json file"""
@@ -233,9 +240,7 @@ def print_text(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Printed!")
         update_stats('text')
     except:
-        logging.error("Failed to print message")
-        update.message.reply_text("Failed printing your message :( Please try sending it again later.")
-        context.bot.send_message(data['admin_id'], text="Failed to print a text message from {}".format(name))
+        error_printing(update, context, name)
 
 def print_photo(update: Update, context: CallbackContext) -> None:
     """Print received image"""
@@ -278,8 +283,7 @@ def print_photo(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Image printed!")
         update_stats('image')
     except:
-        update.message.reply_text("Failed printing your photo :( Please try sending it again later.")
-        context.bot.send_message(data['admin_id'], text="Failed to print an image from {}".format(name))
+        error_printing(update, context, name)
 
 def print_audio(update: Update, context: CallbackContext) -> None:
     """Cannot print received audio"""
@@ -305,8 +309,7 @@ def print_contact(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Contact printed")
         update_stats('contact')
     except:
-        update.message.reply_text("Failed printing your contact :( Please try sending it again later.")
-        context.bot.send_message(data['admin_id'], text="Failed to print a contact from {}".format(name))
+        error_printing(update, context, name)
 
 def print_document(update: Update, context: CallbackContext) -> None:
     """Don't print received document"""
@@ -333,8 +336,7 @@ def print_location(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Location printed")
         update_stats('location')
     except:
-        update.message.reply_text("Failed printing your location :( Please try sending it again later.")
-        context.bot.send_message(data['admin_id'], text="Failed to print a location from {}".format(name))
+        error_printing(update, context, name)
 
 def print_poll(update: Update, context: CallbackContext) -> None:
     """Print received poll"""
@@ -355,8 +357,7 @@ def print_poll(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Poll printed")
         update_stats('poll')
     except:
-        update.message.reply_text("Failed printing your poll :( Please try sending it again later.")
-        context.bot.send_message(data['admin_id'], text="Failed to print a poll from {}".format(name))
+        error_printing(update, context, name)
 
 def print_video(update: Update, context: CallbackContext) -> None:
     """Don't print received video"""
@@ -382,6 +383,9 @@ def main():
 
     # Printer settings
     p.set(align='center')
+    # Print started message
+    p.text("Bragi started!")
+    p.cut()
     # Load saves dict
     global data
     try:
